ccb1fcef47e4c994c9b26b613586e5747beba95d
aintnofastlife <yusukemiyamura2025@gmail.com>
2023-10-10 03:59:06 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/00_JavaScriptの式と演算子/main.js
+++ 01_テキスト/00_JavaScriptの式と演算子/main.js
@@ -1,15 +1,27 @@
 // TRY: 練習問題１
 
 // TRY: 練習問題２
+let x = 5
+x += 2
+x *= 5
+x = 100 % x
+x /= 6
+
+console.log(x)
 
 // TRY: 練習問題３
-let happiness = 0 // 0 から 100 までの数値を代入してください
-let sleepiness = 0 // 0 から 100 までの数値を代入してください
+let happiness = 2 // 0 から 100 までの数値を代入してください
+let sleepiness = 91 // 0 から 100 までの数値を代入してください
 
 const isHappy = happiness > 90
 const isSleepy = sleepiness > 90
 
 const isNotHappyAndSleepy = !isHappy && isSleepy
+
+console.log(isNotHappyAndSleepy)
+console.log(isHappy)
+console.log(isSleepy)
+console.log(isNotHappyAndSleepy)
 
 // 以下のコメントアウトを外して確認してみましょう
 // console.log("isNotHappyAndSleepy" + "は" + true + "ですか？", isNotHappyAndSleepy)
--- 01_テキスト/00_JavaScriptの関数/main.js
+++ 01_テキスト/00_JavaScriptの関数/main.js
@@ -9,11 +9,41 @@ const plus1 = function (x) {
 // TRY: 練習問題 ２
 // f1: 1, 4, 7, 10, 13, 16, ...
 
+const f1 = function (n) {
+  return 3 * n + 1
+}
+
+console.log(f1(3))
+
 // f2: 1, 10, 100, 1000, 10000, ...
 
+const f2 = function (n) {
+  return 10 ** n
+}
+
+console.log(f2(4))
+
 // f3: 2, 14, 107, 1010, 10013, ...
+
+const f3 = function (n) {
+  return f1(n) + f2(n)
+}
+
+console.log(f3(2))
 
 // TRY: 練習問題３
 // ① ２つの引数を受け取り、それらをかけ合わせた値を返す関数 multiply を作成して正しく動くかテストしてください。
 
-// ② ２つの引数を受け取り、それらを足し合わせた結果を四捨五入した値を返す関数 plusRound を作成して正しく動くかテストしてください。
+const multipy = function (x, y) {
+  return x * y
+}
+
+console.log(multipy(6, 7))
+
+// ② ２つの引数を受け取り、それらを足し合わせた結果を四捨五入した値を返す関数 plusRound を作成して正しく動くかテストしてください.
+
+const plusround = function (x, y) {
+  return Math.round(x + y)
+}
+
+console.log(plusround(2.3, 3.8))
