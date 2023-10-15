2a7235064e07e736f04146f05428b0d8b8378924
aintnofastlife <yusukemiyamura2025@gmail.com>
2023-10-15 18:33:48 +0900

    [ストップウォッチ]を学習しました。

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -15,12 +15,26 @@ const hikakin = {
 // TRY: 練習問題１
 // hikakin オブジェクト を真似して自分のプロフィールをオブジェクトで作ってください。
 // オブジェクトにはたくさんのプロパティを追加してください。
+const me = {
+  name: "宮村優輔",
+  age: 24,
+  hobbies: "music",
+  isBored: true,
+  address: {
+    counry: "Japan",
+    city: "Nagoya",
+  },
+  sayHowlow: function () {
+    console.log("Howlowwwww")
+  },
+}
 
 // const me = { name: 'あなたのなまえ' }
 
 // ブラウザのコンソールを開いてオブジェクトが正しく作れているか確認してみましょう👀
 
 // console.log(me)
-
+console.log(me)
 // TRY: 練習問題２
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+me.sayHowlow()
