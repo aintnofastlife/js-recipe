9b508cfd20cd9a9511844b16822133928d5d7f5d
aintnofastlife <yusukemiyamura2025@gmail.com>
2023-10-15 14:48:51 +0900

    [□と○]を学習しました。

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,22 @@
+// button 要素と click イベント
+const button = document.getElementById("button")
+
+const alertMessage = function () {
+  alert("クリックしたね")
+}
+button.onclick = alertMessage
+
+// input 要素と input イベント
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function (e) {
+  console.log(e.target.value)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
+
+document.onkeydown = function (e) {
+  console.log(e.key)
+}
